f2ec347cb420f804403293e9fee6f6dc25278741
Eric Ward <eric.ward@noaa.gov>
2021-09-29 20:49:00 -0700

    catch 0s

--- code/gen_index_by_spp.R
+++ code/gen_index_by_spp.R
@@ -34,16 +34,21 @@ for (i in 1:length(unique_files)) {
   #   "station", "line", "latitude",
   #   "longitude", "time", "scientific_name", "larvae_10m2"
   # ))
-  station_dat <- tabledap(out,
+  station_dat <- tabledap(
+    out,
     fields = c(
-      "larvae_10m2", "latitude", "longitude",
-      "station", "scientific_name", "time"
+      "larvae_10m2",
+      "latitude",
+      "longitude",
+      "station",
+      "scientific_name",
+      "time"
     )
   )
   # filter out species in question
-  species <- dplyr::filter(tot_cpue, erddap==unique_files[i])
+  species <- dplyr::filter(tot_cpue, erddap == unique_files[i])
   dat <- dplyr::filter(station_dat,
-        scientific_name %in% species$scientific_name)
+                       scientific_name %in% species$scientific_name)
 
   # convert date
   dat <- as.data.frame(dat)
@@ -61,18 +66,21 @@ for (i in 1:length(unique_files)) {
   dat$season[which(dat$yday %in% 90:141)] <- 2
   dat$season[which(dat$yday %in% 181:233)] <- 3
   dat$season[which(dat$yday %in% 273:325)] <- 4
-  dat <- dplyr::filter(dat, !is.na(season))
+  dat <- dplyr::filter(dat,!is.na(season))
   dat$season <- as.factor(dat$season)
-  dat <- dplyr::filter(dat, season==2)
+  dat <- dplyr::filter(dat, season == 2)
 
   # convert to UTM - kms
   # make the UTM cols spatial (X/Easting/lon, Y/Northing/lat)
   dat$latitude <- as.numeric(dat$latitude)
   dat$longitude <- as.numeric(dat$longitude)
-  dat <- st_as_sf(dat, coords = c("longitude", "latitude"), crs = 4326)
-  dat<-st_transform(x = dat, crs = 32610)
-  dat$longitude = st_coordinates(dat)[,1]
-  dat$latitude = st_coordinates(dat)[,2]
+  dat <-
+    st_as_sf(dat,
+             coords = c("longitude", "latitude"),
+             crs = 4326)
+  dat <- st_transform(x = dat, crs = 32610)
+  dat$longitude = st_coordinates(dat)[, 1]
+  dat$latitude = st_coordinates(dat)[, 2]
   dat <- as.data.frame(dat)
   dat$longitude <- dat$longitude / 1000 # to kms
   dat$latitude <- dat$latitude / 1000 # to kms
@@ -84,53 +92,56 @@ for (i in 1:length(unique_files)) {
   # remove species with 0 records
   dat = dplyr::group_by(dat, scientific_name) %>%
     dplyr::mutate(tot = sum(larvae_10m2)) %>%
-    dplyr::filter(tot>0) %>%
+    dplyr::filter(tot > 0) %>%
     dplyr::select(-tot)
 
-  # expand predicted grid to have separate rows for each spp
-  new_grid <- expand.grid("scientific_name"=unique(dat$scientific_name),
-              "station"=unique(pred_grid$station))
-  new_grid <- dplyr::left_join(new_grid, pred_grid) %>%
-    dplyr::rename(species = scientific_name) %>%
-    dplyr::filter(year >= min(dat$year), year <= max(dat$year))
+  if (nrow(dat) > 0) {
+    # expand predicted grid to have separate rows for each spp
+    new_grid <-
+      expand.grid(
+        "scientific_name" = unique(dat$scientific_name),
+        "station" = unique(pred_grid$station)
+      )
+    new_grid <- dplyr::left_join(new_grid, pred_grid) %>%
+      dplyr::rename(species = scientific_name) %>%
+      dplyr::filter(year >= min(dat$year), year <= max(dat$year))
 
-  # nest fitted and predicted data
-  dat_nested <-
-    dat %>%
-    dplyr::rename(species = scientific_name) %>%
-    nest(-species) %>%
-    rename(myorigdata = data)
+    # nest fitted and predicted data
+    dat_nested <-
+      dat %>%
+      dplyr::rename(species = scientific_name) %>%
+      nest(-species) %>%
+      rename(myorigdata = data)
 
-  # create second dataset - predictions
-  pred_nested <-
-    new_grid %>%
-    nest(-species) %>%
-    rename(mynewdata = data)
+    # create second dataset - predictions
+    pred_nested <-
+      new_grid %>%
+      nest(-species) %>%
+      rename(mynewdata = data)
 
-  predictions_all <-
-    dat_nested %>%
-    mutate(my_model = map(myorigdata, gam_fit)) %>%
-    full_join(pred_nested, by = "species") %>%
-    mutate(my_new_pred = map2(my_model, mynewdata, predict)) %>%
-    select(species, mynewdata, my_new_pred) %>%
-    unnest(mynewdata, my_new_pred) %>%
-    dplyr::group_by(species, year) %>%
-    dplyr::summarise(index = log(sum(exp(my_new_pred)))) %>%
-    as.data.frame()
+    predictions_all <-
+      dat_nested %>%
+      mutate(my_model = map(myorigdata, gam_fit)) %>%
+      full_join(pred_nested, by = "species") %>%
+      mutate(my_new_pred = map2(my_model, mynewdata, predict)) %>%
+      select(species, mynewdata, my_new_pred) %>%
+      unnest(mynewdata, my_new_pred) %>%
+      dplyr::group_by(species, year) %>%
+      dplyr::summarise(index = log(sum(exp(my_new_pred)))) %>%
+      as.data.frame()
 
-  # also calculate summaries from data
-  summaries <- dplyr::rename(dat, species = scientific_name) %>%
-    dplyr::group_by(species, year) %>%
-    dplyr::summarize(
-      mean_cpue = mean(larvae_10m2),
-      n_pos_cpue = length(which(larvae_10m2 > 0))
-    )
-  predictions_all <- dplyr::left_join(predictions_all, summaries)
+    # also calculate summaries from data
+    summaries <- dplyr::rename(dat, species = scientific_name) %>%
+      dplyr::group_by(species, year) %>%
+      dplyr::summarize(mean_cpue = mean(larvae_10m2),
+                       n_pos_cpue = length(which(larvae_10m2 > 0)))
+    predictions_all <- dplyr::left_join(predictions_all, summaries)
 
-  if (i == 1) {
-    all_pred <- predictions_all
-  } else {
-    all_pred <- rbind(all_pred, predictions_all)
+    if (i == 1) {
+      all_pred <- predictions_all
+    } else {
+      all_pred <- rbind(all_pred, predictions_all)
+    }
   }
 }
 
