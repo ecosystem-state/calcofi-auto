533a5ecae53e002f0de267de8f68b50d270cd93d
Eric Ward <5046884+ericward-noaa@users.noreply.github.com>
2026-03-02 09:37:04 -0800

    reorganize

--- code/archive/gen_index.R
+++ code/archive/gen_index.R
@@ -1,176 +1,108 @@
-library(mgcv)
-library(rerddap)
+library(sdmTMB)
 library(dplyr)
 library(lubridate)
-library(sp)
 library(tidyr)
-library(purrr)
+library(curl)
+
 # settings
-top_species <- 50
-pred_resolution <- 5 # resolution of prediction grid, km
-#
-# list of all datasets
-calcofi_erddap <- c(
-  "erdCalCOFIlrvcntAtoAM",
-  "erdCalCOFIlrvcntANtoAR",
-  "erdCalCOFIlrvcntAStoBA",
-  "erdCalCOFIlrvcntBCEtoBZ",
-  "erdCalCOFIlrvcntCtoCE",
-  "erdCalCOFIlrvcntCDtoCH",
-  "erdCalCOFIlrvcntCItoCO",
-  "erdCalCOFIlrvcntCPtoDE",
-  "erdCalCOFIlrvcntDHtoEC",
-  "erdCalCOFIlrvcntEDtoEU",
-  "erdCalCOFIlrvcntEVtoGN",
-  "erdCalCOFIlrvcntGOtoHA",
-  "erdCalCOFIlrvcntHBtoHI",
-  "erdCalCOFIlrvcntHJtoID",
-  "erdCalCOFIlrvcntIEtoLA",
-  "erdCalCOFIlrvcntLBtoLI",
-  "erdCalCOFIlrvcntLJtoMA",
-  "erdCalCOFIlrvcntMBtoMO",
-  "erdCalCOFIlrvcntMPtoNA",
-  "erdCalCOFIlrvcntNBtoOL",
-  "erdCalCOFIlrvcntOMtoOX",
-  "erdCalCOFIlrvcntOYtoPI",
-  "erdCalCOFIlrvcntPLtoPO",
-  "erdCalCOFIlrvcntPPtoPZ",
-  "erdCalCOFIlrvcntQtoSA",
-  "erdCalCOFIlrvcntSBtoSC",
-  "erdCalCOFIlrvcntSDtoSI",
-  "erdCalCOFIlrvcntSJtoST",
-  "erdCalCOFIlrvcntSUtoTE",
-  "erdCalCOFIlrvcntTFtoU",
-  "erdCalCOFIlrvcntVtoZ"
-)
+source("code/set_control_params.R")
 
-# grab data for all species
-for (i in 1:length(calcofi_erddap)) {
-  out <- info(as.character(calcofi_erddap[i]))
-  # station_dat <- tabledap(out, fields = c(
-  #   "station", "line", "latitude",
-  #   "longitude", "time", "scientific_name", "larvae_10m2"
-  # ))
-  station_dat <- tabledap(out,
-    fields = c(
-      "larvae_10m2", "latitude", "longitude",
-      "station", "scientific_name", "time"
-    )
-  )
+# load prediction grid
+pred_grid <- readRDS("indices/pred_grid.rds")
+pred_grid$season <- 2
+pred_grid$yday <- 105 # Apr 15
+pred_grid$fyear <- as.factor(pred_grid$year)
+pred_grid <- dplyr::rename(pred_grid, X = longitude, Y = latitude) |>
+  dplyr::filter(year == 2020)
+pred_grid <- sdmTMB::replicate_df(pred_grid, time_name = "year", time_values = 1985:2023)
+# model function - edit to change form of model
+# gam_fit <- function(df) {
+#   gam(larvae_10m2 ~ as.factor(year) + s(latitude, longitude, by = year),
+#       data = df,
+#       family = tw()
+#   )
+# }
 
-  if (i == 1) {
-    dat <- station_dat
-  } else {
-    dat <- rbind(dat, station_dat)
-  }
-}
+#url_str <- "https://github.com/ecosystem-state/ecodata/blob/main/inst/calcofi_index_data.rds"
+#usethis::use_github_file(url_str,
+#                         save_as = "data/index_data.rds")
 
-# dataset is very large; > 70 million rows
-dat <- as.data.frame(dat)
-dat$date <- lubridate::as_date(dat$time)
-dat$year <- lubridate::year(dat$date)
-dat$month <- lubridate::month(dat$date)
-dat$yday <- lubridate::yday(dat$date)
+githubURL <- ("https://raw.githubusercontent.com/ecosystem-state/ecodata/main/inst/calcofi_index_data.rds")
+download.file(githubURL,"data/index_data.rds", method="curl")
 
-# filter out recent years with consistent sampling
-dat <- dplyr::filter(dat, year >= 1985)
+dat = readRDS("data/index_data.rds") |>
+  dplyr::filter(larvae_10m2 > 0)
+  
+# create unique grid 
+station_dat <- dplyr::group_by(dat, station, line, latitude, longitude,
+                               time, date, year, month, yday, lat_dd, lon_dd) |>
+  dplyr::summarise(n = n()) |> 
+  dplyr::ungroup() |>
+  dplyr::mutate(zday = (yday - mean(yday)) / sd(yday))
 
-# # time windows https://calcofi.org/cruises.html
-dat$season <- NA
-dat$season[which(dat$yday %in% 2:52)] <- 1
-dat$season[which(dat$yday %in% 90:141)] <- 2
-dat$season[which(dat$yday %in% 181:233)] <- 3
-dat$season[which(dat$yday %in% 273:325)] <- 4
-dat <- dplyr::filter(dat, !is.na(season))
-dat$season <- as.factor(dat$season)
+species_names = c(names(sort(table(dat$scientific_name), decreasing = TRUE)))
 
-# Filter out experimental stations
-# https://calcofi.org/field-work/station-positions.html
-stations <- read.csv("data/CalCOFIStationOrder.csv")
-stations <- dplyr::rename(stations, station = Station)
-dat <- dplyr::left_join(dat, stations[, c("station", "StaType")])
-dat <- dplyr::filter(dat, StaType == "ROS")
+df = data.frame(species_names = species_names,
+                spatiotemporal1 = "iid",
+                spatiotemporal2 = "off")
+# df is ordered by species, data rich to poor. many data poor species
+# can't support spatiotemporal pos fields
+df$spatiotemporal1[4] <- "off"
+df$spatiotemporal1[10] <- "off"
+df$spatiotemporal1[16] <- "off"
+df$spatiotemporal1[20:nrow(df)] <- "off"
 
-dat$latitude <- as.numeric(dat$latitude)
-dat$longitude <- as.numeric(dat$longitude)
+for(spp in 1:nrow(df)) {
 
-# convert to UTM - kms
-sp::coordinates(dat) <- c("longitude", "latitude")
-sp::proj4string(dat) <- sp::CRS("+proj=longlat + ellps=WGS84 +datum=WGS84")
-dat <- try(sp::spTransform(dat, CRS = "+proj=utm +zone=10 +datum=WGS84"),
-  silent = TRUE
-)
-dat <- as.data.frame(dat)
-dat$longitude <- dat$longitude / 1000
-dat$latitude <- dat$latitude / 1000
+    # fit sdmTMB model
+    newdat <- dplyr::filter(dat,
+                            scientific_name==df$species_names[spp])
 
-# format response
-dat$larvae_10m2 <- as.numeric(dat$larvae_10m2)
-dat$larvae_10m2[which(is.na(dat$larvae_10m2))] <- 0
+    dat_summary <- dplyr::left_join(station_dat, newdat)
+    dat_summary$larvae_10m2[which(is.na(dat_summary$larvae_10m2))] <- 0
+    dat_summary <- add_utm_columns(dat_summary, ll_names = c("lon_dd", "lat_dd"))
+      #if(spp == 1) {
+        # 10 ~ 181 knots
+        mesh = make_mesh(dat_summary, xy_cols = c("X","Y"),
+                         cutoff = 50)
+      #}
 
-# filtering of top species by
-top_cpue <- dplyr::group_by(dat, scientific_name) %>%
-  dplyr::summarise(tot = sum(larvae_10m2[which(season == 2)])) %>%
-  dplyr::arrange(-tot)
-top_cpue <- top_cpue[1:top_species, ]
-dat <- dplyr::filter(dat, scientific_name %in% top_cpue$scientific_name)
+      dat_summary$fyear = as.factor(dat_summary$year)
+      #newdat$present = ifelse(newdat$larvae_10m2 > 0, 1, 0)
+      m <- sdmTMB(larvae_10m2 ~ -1 + s(yday) + fyear,
+                  spatiotemporal = list(df$spatiotemporal1[spp], df$spatiotemporal2[spp]),
+                  time="year",
+                  spatial="on",
+                  family = delta_lognormal(),
+                  mesh=mesh,
+                  share_range = TRUE,
+                  data=dat_summary,
+                  priors = sdmTMBpriors(matern_s = pc_matern(range_gt = 5, sigma_lt = 1)))
 
-# come up with prediction grid
-resolution <- pred_resolution
-dat$floor_lon <- floor(dat$longitude / resolution)
-dat$floor_lat <- floor(dat$latitude / resolution)
-dat$station <- paste(dat$floor_lon, dat$floor_lat)
+      if(sanity(m, silent=TRUE)$all_ok == TRUE) {
 
-pred_grid <- expand.grid(
-  station = unique(dat$station),
-  season = 2,
-  year = unique(dat$year),
-  species = unique(dat$scientific_name)
-)
-station_df <- data.frame(station = unique(dat$station))
-station_df$longitude <- as.numeric(unlist(lapply(strsplit(as.character(station_df$station), " "), getElement, 1)))
-station_df$latitude <- as.numeric(unlist(lapply(strsplit(as.character(station_df$station), " "), getElement, 2)))
-pred_grid <- dplyr::left_join(pred_grid, station_df)
+        
+        pred = predict(m,
+                       pred_grid,
+                       return_tmb_object = TRUE)
+        index = get_index(pred, bias_correct = TRUE) 
+  
+        summaries <- dat_summary |> 
+          dplyr::group_by(year) %>%
+          dplyr::summarize(mean_cpue = mean(larvae_10m2),
+                           n_pos_cpue = length(which(larvae_10m2 > 0)))
+        summaries$species <- newdat$common_name[1]
+        pred = left_join(summaries, index[,c("year","est","lwr","upr","log_est","se")])
+  
+        if(spp == 1) {
+          predictions_all = pred
+        } else {
+          predictions_all = rbind(predictions_all, pred)
+        }
+        saveRDS(predictions_all, "indices/predicted_indices_sdmtmb.rds")
+      }
+      print(spp)
+} # end spp loop
 
-# model function
-gam_fit <- function(df) {
-  gam(larvae_10m2 ~ as.factor(year) + s(latitude, longitude, by = year),
-    data = df,
-    family = tw()
-  )
-}
 
-# nest fitted and predicted data
-dat_nested <-
-  dat %>%
-  dplyr::rename(species = scientific_name) %>%
-  nest(-species) %>%
-  rename(myorigdata = data)
 
-# create second dataset - predictions
-pred_nested <-
-  pred_grid %>%
-  nest(-species) %>%
-  rename(mynewdata = data)
-
-predictions_all <-
-  dat_nested %>%
-  mutate(my_model = map(myorigdata, gam_fit)) %>%
-  full_join(pred_nested, by = "species") %>%
-  mutate(my_new_pred = map2(my_model, mynewdata, predict)) %>%
-  select(species, mynewdata, my_new_pred) %>%
-  unnest(mynewdata, my_new_pred) %>%
-  dplyr::group_by(species, year) %>%
-  dplyr::summarise(index = log(sum(exp(my_new_pred)))) %>%
-  as.data.frame()
-
-# also calculate summaries from data
-summaries <- dplyr::rename(dat, species = scientific_name) %>%
-  dplyr::group_by(species, year) %>%
-  dplyr::summarize(
-    mean_cpue = mean(larvae_10m2),
-    n_pos_cpue = length(which(larvae_10m2 > 0))
-  )
-predictions_all <- dplyr::left_join(predictions_all, summaries)
-
-saveRDS(predictions_all, "indices/predicted_indices.rds")
